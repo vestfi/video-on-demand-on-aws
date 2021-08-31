49edcf5c23bd7025adb1d0ee2b096c93ae09662f
Frans Ramirez <franscaraveli@gmail.com>
2021-08-31 13:49:20 -0500

    adding medioinfo

--- source/mediainfo/setup.py
+++ source/mediainfo/setup.py
@@ -17,12 +17,14 @@ from setuptools.command.test import test as TestCommand
 from setuptools import Command
 import subprocess
 
+
 def run_bash_command(command):
     try:
         subprocess.check_call([command], shell=True)
     except subprocess.CalledProcessError as error:
         print(f'Command failed with exit code: {error.returncode}')
         sys.exit(error.returncode)
+
 
 class BuildPackageCommand(Command):
     description = 'Build lambda package'
@@ -39,6 +41,7 @@ class BuildPackageCommand(Command):
     def run(self):
         run_bash_command(f'zip -rq9 {self.zip_path} lambda_function.py ./bin/*')
 
+
 class UnitTestsCommand(TestCommand):
     description = 'Run unit tests'
 
@@ -47,6 +50,7 @@ class UnitTestsCommand(TestCommand):
         run_bash_command('cp lambda_function.py ./test*.py ./pytests')
         run_bash_command('python3 -m unittest discover -s ./pytests -v')
         run_bash_command('rm -rf ./pytests')
+
 
 setup(
     name='mediainfo-function',
